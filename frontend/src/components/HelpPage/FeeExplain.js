5625f0fc9001eab6a3f7594b9e4c8f2a7498ca0e
Natsatika <Natsatika.P@ku.th>
2024-06-27 00:33:04 +0700

    add content to FeeExplainPage

--- frontend/src/components/HelpPage/FeeExplain.js
+++ frontend/src/components/HelpPage/FeeExplain.js
@@ -45,10 +45,34 @@ class FeeExplain extends React.Component {
     return (
         <div className="container-lg">
             <div className="head-spacer"></div>
-            <h1 className="page-title align-left">Why do I have to pay Fee?</h1>
+            <h1 className="page-title align-left">Why do I have to pay <span className="color-accent">Fee</span>?</h1>
             <div className="row">
-
+              <div className="col-sm-10 px-3 text-start">
+                <p className="h3 my-5 fee-explain-text">
+                To ensure that transactions on our platform are transparent and verifiable, we choose <span className="color-avax fw-bold">Avalanche blockchain</span> to record your tickets and transactions.
+                </p>
+                <p className="h3 my-5 fee-explain-text">
+                When recording transactions or creating tickets in the form of NFTs on the blockchain, there will be a fee to be paid to the system as an <span className="color-accent fw-bold">operation cost</span>, which will vary <u>depending on type of action and how busy of the system at the moment</u>.
+                </p>
+                <p className="h3 my-5 fee-explain-text">
+                But don’t worry, we will always notify you of these Network fees before you decide to make a transaction.
+                </p>
+              </div>
             </div>
+            <div className="row mb-5">
+              <div className="col px-3 text-start">
+                <p className="h2 my-5 fw-bold">
+                Fees will be charged when you carry out these types of transactions:
+                </p>
+                <ul className="h3 fee-explain-list">
+                  <li>Buying tickets</li>
+                  <li>Canceling ticket sales</li>
+                  <li>Redeeming tickets</li>
+                  <li>Reselling tickets</li>
+                </ul>
+              </div>
+            </div>
+            <div className="head-spacer"></div>
         </div>
     )
   }
